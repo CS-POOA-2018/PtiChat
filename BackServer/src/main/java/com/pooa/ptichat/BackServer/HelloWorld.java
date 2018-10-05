fe66c4fa3bbdd0e035b59e5dded167ead2a23635
Come de Cerval <come.decerval@student.ecp.fr>
2018-10-05 11:08:13 +0200

    Start test

---
+++ BackServer/src/main/java/com/pooa/ptichat/BackServer/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.pooa.ptichat.BackServer;
+
+public class HelloWorld {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
