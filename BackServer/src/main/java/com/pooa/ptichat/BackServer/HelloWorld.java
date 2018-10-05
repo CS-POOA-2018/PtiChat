9ef8c251b216ca45483d82ea57d550b3e1d1fdbe
Come de Cerval <come.decerval@student.ecp.fr>
2018-10-05 11:22:04 +0200

    [BACK] Add a new route

--- BackServer/src/main/java/com/pooa/ptichat/BackServer/HelloWorld.java
+++ BackServer/src/main/java/com/pooa/ptichat/BackServer/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public HelloWorld(long id, String content) {
         this.id = id;
         this.content = content;
     }
