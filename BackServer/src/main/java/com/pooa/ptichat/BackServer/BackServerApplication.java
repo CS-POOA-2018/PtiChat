79e41f291e402f36c3a28e3aca8dee81525ff5e0
Come de Cerval <come.decerval@student.ecp.fr>
2018-10-05 10:48:04 +0200

    Added back server spring template

---
+++ BackServer/src/main/java/com/pooa/ptichat/BackServer/BackServerApplication.java
@@ -0,0 +1,12 @@
+package com.pooa.ptichat.BackServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackServerApplication.class, args);
+	}
+}
