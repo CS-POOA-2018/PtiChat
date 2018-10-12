1b0dd46913d89f4685b3e7aa9b2934eb4ea31a48
Tishwa <solen.leroux.c@gmail.com>
2018-10-12 11:48:28 +0200

    [App] Add Login page

---
+++ PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package fr.centralesupelec.ptichatapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onConnect(View view) {
+        // switch activity to main
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
