6f394da9db39f1b453e7f6bc62e03e4baeec368c
Tishwa <solen.leroux.c@gmail.com>
2018-10-16 15:14:07 +0200

    [App] Username now changes depending on connexion

--- PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/MainActivity.java
+++ PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    private TextView userName;
+    private TextView userStatus;
+    private TextView userIsOnline;
 
     // TODO: this is mock data, get real data
     private User[] myDataset = {
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.mainRecyclerView);
+        recyclerView = findViewById(R.id.mainContactView);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -36,5 +41,35 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyAdapter(myDataset);
         recyclerView.setAdapter(adapter);
 
+        // get the textViews
+        userName = findViewById(R.id.mainName);
+        userStatus = findViewById(R.id.mainStatus);
+        userIsOnline = findViewById(R.id.mainIsOnline);
+
+        // update user
+        updateUser();
+    }
+
+    private void updateUser() {
+        User currentUser = Session.getUser();
+        updateName(currentUser.getPseudo());
+        updateStatus(currentUser.getStatus());
+        updateOnline(currentUser.isConnected());
+    }
+
+    private void updateName(String pseudo) {
+        if (!pseudo.isEmpty()) {
+            userName.setText(pseudo);
+        }
+    }
+
+    private void updateStatus(String status) {
+        if (!status.isEmpty()) {
+            userStatus.setText(status);
+        }
+    }
+
+    private void updateOnline(Boolean isConnected) {
+        userIsOnline.setText(isConnected ? "(En ligne)" : "(Hors ligne)");
     }
 }
---
+++ PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/Session.java
@@ -0,0 +1,19 @@
+package fr.centralesupelec.ptichatapp;
+
+class Session {
+    private static User user;
+
+    public static Boolean connect(String login, String password) {
+        // TODO : send it to a real backend
+        setUser(new User(login, login, "", true));
+        return true;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    private static void setUser(User user) {
+        Session.user = user;
+    }
+}
