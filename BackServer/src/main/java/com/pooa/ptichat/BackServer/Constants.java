78b2e98073287c81e19b91b0160c5488ed5aec36
aymeric <aymeric@feedly.com>
2018-10-17 00:39:55 +0200

    [BackServer] Date to String and reverse function in Utils class

--- BackServer/src/main/java/com/pooa/ptichat/BackServer/Constants.java
+++ BackServer/src/main/java/com/pooa/ptichat/BackServer/Constants.java
@@ -2,4 +2,5 @@ package com.pooa.ptichat.BackServer;
 
 public class Constants {
     public static final String SQLITE_PATH = "/Users/aymeric/Cours/POOA/PtiChat_Database";
+    public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
 }
