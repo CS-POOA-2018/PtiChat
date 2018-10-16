5f2c348b79c419d38ba544579a0cf0b387fbd427
Tishwa <solen.leroux.c@gmail.com>
2018-10-16 16:45:24 +0200

    [App] Add Message and Chat classes

--- PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/Chat.java
+++ PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/Chat.java
@@ -1,4 +1,29 @@
 package fr.centralesupelec.ptichatapp;
 
 public class Chat {
+
+    private String id;
+    private String name;
+
+    public Chat(String id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/Message.java
+++ PtiChatApp/app/src/main/java/fr/centralesupelec/ptichatapp/Message.java
@@ -1,4 +1,71 @@
 package fr.centralesupelec.ptichatapp;
 
+import java.util.Date;
+
 public class Message {
+
+    private User sender;
+    private Chat chat;
+    private String id;
+    private Date date;
+    private String content;
+    private Boolean hasBeenRead;
+
+    public Message(User sender, Chat chat, String id, Date date, String content, Boolean hasBeenRead) {
+        setSender(sender);
+        setChat(chat);
+        setId(id);
+        setDate(date);
+        setContent(content);
+        setHasBeenRead(hasBeenRead);
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getHasBeenRead() {
+        return hasBeenRead;
+    }
+
+    public void setHasBeenRead(Boolean hasBeenRead) {
+        this.hasBeenRead = hasBeenRead;
+    }
+
 }
